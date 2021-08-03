b8b8164ed88399a8b62fde0237ee78d7565a2482
Sébastien Krafft <skrafft@gmail.com>
2021-08-03 18:02:31 +0200

    ERC677 and terms

--- test/token/ShareBridgeToken.test.js
+++ test/token/ShareBridgeToken.test.js
@@ -50,13 +50,13 @@ const YesNoRule = Contracts.getFromLocal('YesNoRule');
 const YesNoUpdateRule = Contracts.getFromLocal('YesNoUpdateRule');
 
 const zero = '0x0000000000000000000000000000000000000000';
-const INITIAL_TOKENIZED_SHARE_PERCENTAGE = '5528' // 55.28%
-const NEW_TOKENIZED_SHARE_PERCENTAGE = '6745' // 67.45%
+const INITIAL_TOKENIZED_SHARES = '5528'
+const NEW_TOKENIZED_SHARES = '6745'
 const BOARD_DOCUMENT_URL = 'https://www.mtpelerin.com/board-resolution.pdf';
 const BOARD_DOCUMENT_HASH = '0x175fca35aac7905c1f5c599ae921ed0e19921406cd117be5825717c8c3357b49';
 const ZERO_HASH = '0x0000000000000000000000000000000000000000000000000000000000000000';
 
-contract('ShareBridgeToken', function ([_, owner, administrator, trustedIntermediary1, trustedIntermediary2, votingSession, address2]) {
+contract('ShareBridgeToken', function ([_, owner, administrator, trustedIntermediary1, trustedIntermediary2, address2]) {
   beforeEach(async function () {
     this.project = await TestHelper();
     this.ruleEngine = await this.project.createProxy(RuleEngine, {initArgs: [owner]});
@@ -64,7 +64,7 @@ contract('ShareBridgeToken', function ([_, owner, administrator, trustedIntermed
     this.yesNoUpdate = await this.project.createProxy(YesNoUpdateRule);
     await this.ruleEngine.methods.setRules([this.yesNo.address, this.yesNoUpdate.address]).send({from: owner, gas: 100000});
     this.processor = await this.project.createProxy(Processor, {initArgs: [owner, this.ruleEngine.address], gas: 100000});
-    this.contract = await this.project.createProxy(Contract, {initArgs: [owner, this.processor.address, 'Test token', 'TST', [trustedIntermediary1, trustedIntermediary2], INITIAL_TOKENIZED_SHARE_PERCENTAGE], gas: 100000});
+    this.contract = await this.project.createProxy(Contract, {initArgs: [owner, this.processor.address, 'Test token', 'TST', [trustedIntermediary1, trustedIntermediary2], INITIAL_TOKENIZED_SHARES], gas: 100000});
   });
 
   context('When administrator', function () {
@@ -72,16 +72,24 @@ contract('ShareBridgeToken', function ([_, owner, administrator, trustedIntermed
       await this.contract.methods.addAdministrator(administrator).send({from: owner});
     });
 
-    it('can set the tokenized share percentage', async function () {
-      (await this.contract.methods.tokenizedSharePercentage().call()).should.equal(INITIAL_TOKENIZED_SHARE_PERCENTAGE);
-      ({events: this.events} = await this.contract.methods.setTokenizedSharePercentage(NEW_TOKENIZED_SHARE_PERCENTAGE).send({from: administrator}));
-      (await this.contract.methods.tokenizedSharePercentage().call()).should.equal(NEW_TOKENIZED_SHARE_PERCENTAGE);
+    it('can set the tokenized shares', async function () {
+      (await this.contract.methods.tokenizedShares().call()).should.equal(INITIAL_TOKENIZED_SHARES);
+      ({events: this.events} = await this.contract.methods.setTokenizedShares(NEW_TOKENIZED_SHARES).send({from: administrator}));
+      (await this.contract.methods.tokenizedShares().call()).should.equal(NEW_TOKENIZED_SHARES);
+      const data = await web3.eth.getStorageAt(this.contract.address, 315);
+      data.should.equal('0x1a59'); // 6745 in hex
     }); 
 
-    it('emits a TokenizedSharePercentageSet event', function () {
-      this.events.should.have.property('TokenizedSharePercentageSet');
-      this.events.TokenizedSharePercentageSet.returnValues.should.have.property('tokenizedSharePercentage', NEW_TOKENIZED_SHARE_PERCENTAGE);
+    it('emits a TokenizedShares event', function () {
+      this.events.should.have.property('TokenizedShares');
+      this.events.TokenizedShares.returnValues.should.have.property('tokenizedShares', NEW_TOKENIZED_SHARES);
     });
+
+    it('can set the tokenization terms', async function () {
+      (await this.contract.methods.terms().call()).should.equal("");
+      (await this.contract.methods.setTerms("https://mtpelerin.com/terms").send({from: administrator}));
+      (await this.contract.methods.terms().call()).should.equal("https://mtpelerin.com/terms");
+    }); 
 
     it('can set the board resolution document attributes', async function () {
       (await this.contract.methods.boardResolutionDocumentUrl().call()).should.equal("");
@@ -89,69 +97,33 @@ contract('ShareBridgeToken', function ([_, owner, administrator, trustedIntermed
       ({events: this.events} = await this.contract.methods.setBoardResolutionDocument(BOARD_DOCUMENT_URL, BOARD_DOCUMENT_HASH).send({from: administrator, gas: 200000}));
       (await this.contract.methods.boardResolutionDocumentUrl().call()).should.equal(BOARD_DOCUMENT_URL);
       (await this.contract.methods.boardResolutionDocumentHash().call()).should.equal(BOARD_DOCUMENT_HASH);
+      const data = await web3.eth.getStorageAt(this.contract.address, 313);
+      data.should.equal(BOARD_DOCUMENT_HASH);
     }); 
 
     it('emits a BoardResolutionDocumentSet event', function () {
       this.events.should.have.property('BoardResolutionDocumentSet');
       this.events.BoardResolutionDocumentSet.returnValues.should.have.property('boardResolutionDocumentHash', BOARD_DOCUMENT_HASH);
     });
-
-    it('can set the voting session contract address', async function () {
-      (await this.contract.methods.votingSession().call()).should.equal(zero);
-      ({events: this.events} = await this.contract.methods.setVotingSession(votingSession).send({from: administrator}));
-      this.votingSession = votingSession;
-      (await this.contract.methods.votingSession().call()).should.equal(votingSession);
-    }); 
-
-    it('emits a VotingSessionSet event', function () {
-      this.events.should.have.property('VotingSessionSet');
-      this.events.VotingSessionSet.returnValues.should.have.property('votingSession', this.votingSession);
-    });
-
-    it('can unset (set to 0) the voting session contract address', async function () {
-      (await this.contract.methods.votingSession().call()).should.equal(zero);
-      await this.contract.methods.setVotingSession(votingSession).send({from: administrator});
-      (await this.contract.methods.votingSession().call()).should.equal(votingSession);
-      ({events: this.events} = await this.contract.methods.setVotingSession(zero).send({from: administrator}));
-      (await this.contract.methods.votingSession().call()).should.equal(zero);
-    }); 
-
-    it('emits a VotingSessionSet event', function () {
-      this.events.should.have.property('VotingSessionSet');
-      this.events.VotingSessionSet.returnValues.should.have.property('votingSession', zero);
-    });
   });
 
   context('When standard user', function () {
     it('can get the contract version', async function () {
-      (await this.contract.methods.VERSION().call()).should.equal('2');
+      (await this.contract.methods.VERSION().call()).should.equal('3');
     });
 
     context('Security model', function () {
-      it('reverts if trying to set the tokenized share percentage', async function () {
-        await shouldFail.reverting.withMessage(this.contract.methods.setTokenizedSharePercentage(NEW_TOKENIZED_SHARE_PERCENTAGE).send({from: address2}), "AD01");
+      it('reverts if trying to set the tokenized shares', async function () {
+        await shouldFail.reverting.withMessage(this.contract.methods.setTokenizedShares(NEW_TOKENIZED_SHARES).send({from: address2}), "AD01");
+      });
+
+      it('reverts if trying to set the tokenization terms', async function () {
+        await shouldFail.reverting.withMessage(this.contract.methods.setTerms("https://mtpelerin.com/terms").send({from: address2}), "AD01");
       });
 
       it('reverts if trying to set the board resolution document', async function () {
         await shouldFail.reverting.withMessage(this.contract.methods.setBoardResolutionDocument(BOARD_DOCUMENT_URL, BOARD_DOCUMENT_HASH).send({from: address2}), "AD01");
       });
-
-      it('reverts if trying to set the voting session contract address', async function () {
-        await shouldFail.reverting.withMessage(this.contract.methods.setVotingSession(votingSession).send({from: address2}), "AD01");
-      });
-    });
-  });
-
-  context("Storage slots positions", function () {
-    it('retains original slot for boardResolutionDocumentHash', async function () {
-      await this.contract.methods.addAdministrator(administrator).send({from: owner});
-      (await this.contract.methods.boardResolutionDocumentUrl().call()).should.equal("");
-      (await this.contract.methods.boardResolutionDocumentHash().call()).should.equal(ZERO_HASH);
-      await this.contract.methods.setBoardResolutionDocument(BOARD_DOCUMENT_URL, BOARD_DOCUMENT_HASH).send({from: administrator, gas: 200000});
-      (await this.contract.methods.boardResolutionDocumentUrl().call()).should.equal(BOARD_DOCUMENT_URL);
-      (await this.contract.methods.boardResolutionDocumentHash().call()).should.equal(BOARD_DOCUMENT_HASH);
-      const data = await web3.eth.getStorageAt(this.contract.address, 313);
-      data.should.equal(BOARD_DOCUMENT_HASH);
     });
   });
 });
